8b64c2bf143ff4cdb4c9df5587fc46e6c4e3a11b
JirkAss <jirk45@gmail.com>
2013-05-05 17:01:44 +0200

    Rzdělány šablony loginu a hlavní stránky

---
+++ src/view/sabMainPage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!-- Šablona je IN PROGRESS (J) -->
+<html>
+    <head lang="cs">
+        <meta name="robots" content="Index, Follow" />
+        <meta charset="UTF-8">
+        <meta name="keywords" content="fórum forum diskuze lidé" />
+        <meta name="description" content="Fórum o všem, co chceš" />
+        <meta name="author" content="Zdeněk Potměšil, Jiří Poláček" />
+        <link href="/web/css/main.css" rel="stylesheet" type="text/css" />
+        
+        
+        <title>sabMainPage</title>
+
