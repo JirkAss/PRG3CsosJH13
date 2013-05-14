58392c08cc8ef6d9c9cfb1f1d3713274386f15c2
JirkAss <jirk45@gmail.com>
2013-05-14 12:50:53 +0200

    vylepšná login šablona s vyborným vyskakovacím popupem registrace

--- src/view/sabLogin.php
+++ src/view/sabLogin.php
@@ -7,7 +7,7 @@
         <meta name="keywords" content="fórum forum diskuze lidé" />
         <meta name="description" content="Fórum o všem, co chceš" />
         <meta name="author" content="Zdeněk Potměšil, Jiří Poláček" />
-        <link href="/web/css/main.css" rel="stylesheet" type="text/css" />
+        <link href="css/css1.css" rel="stylesheet" type="text/css" />
         
         
         <title>Login Pyčo</title>
@@ -29,7 +29,53 @@
 
     <div><input type="submit" name=\"go\" value=\"Login\"></div>
 <!--  tlačítko ( type=\"submit\" ) odesíla data do souboru overeni.php  -->
+<a href="#openModal"><i>Chci se registrovat</i></a>
 
+<div id="openModal" class="modalDialog">
+	<div>
+		<a href="#close" title="Close" class="close">X</a>
+		<strong>Toto je šablona Registrace sabReg.php</strong>
+        <h1>Registrace nového uživatele</h1>
+        <form action="ulozeni.php" method="POST">
+<!--  pridavani.php je skript pro přidávaní nových uživatelů do databáze, tzn. po klíknutí na tlačítko Ok se data pošlou do tohoto souboru metodou $_POST  -->
+    
+        <div>
+            <label for="nickname">Nickname:</label>
+        </div> 
+
+        <div>
+            <input type="text" name="nickname">
+        </div>
+<!--  Do textového pole se jménem name ( name="nickname" ) nový uživatel zadá svoje nickname  -->
+
+        <div>
+            <label for="e-mail">e-mail:</label>
+        </div>
+
+        <div>
+            <input type="text" name="e-mail">
+        </div>
+<!--  Do textového pole se jménem login ( name="e-mail" ) nový uživatel zadá svůj e-mail  -->
+    
+        <div>
+            <label for="password">Heslo:</label>
+        </div>
+
+        <div>
+            <input type="password" name="password">
+        </div>
+<!--  Do pole se jménem heslo ( name="password" ) nový uživatel zadá svoje heslo  -->
+
+        <div>
+            <input type="submit" name="go" value="Ok">
+        </div>
+<!--  tlačítko ( type="submit" ) odesíla data do souboru pridavani.php  -->
+
+</form>
+
+<div class="reg"> <a href="index.php">přihlásit se</a></div>
+	</div>
+</div>
 </form>
 </div>   
             
