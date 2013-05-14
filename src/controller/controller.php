533cfb2c834ba7de8d97c434ec3b255f9db42a54
JirkAss <jirk45@gmail.com>
2013-05-14 12:50:05 +0200

    nová metoda pro zpracování šablony loginu

--- src/controller/controller.php
+++ src/controller/controller.php
@@ -12,5 +12,10 @@ class controller
        extract($this->data);
        require_once '../src/view/sabLogin.php';
     }
+    public function ukazReg()
+    {
+        extract($this->data);
+        require_once '../src/view/sabReg.php';
+    }
 }
 ?>
