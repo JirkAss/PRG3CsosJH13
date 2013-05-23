be9afe4459f53b0e29843f493f3168a44cb786cc
RadekOberreiter <oberreiterradek@gmail.com>
2013-05-23 12:48:04 +0200

    Obsah fóra

---
+++ obsah_fora.php
@@ -0,0 +1,34 @@
+<html>
+    
+    <head>
+    <title>Obsah fora</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="css/obsah_fora.css">
+    </head>
+    <body>
+        <h1>Témata fóra</h1>
+        
+        <a href=""> <h2>Hry</h2> </a>
+        <div> Hry od roku 2000 - 201? 
+            <br>    &nbsp;
+        </div>
+        
+        <a href=""><h2>Filmy</h2></a>
+        <div> Filmy od roku 2000 - 201?
+        <br>    &nbsp;
+        </div>
+        
+       <a href=""> <h2>Hudba</h2></a>
+        <div> Hudba od roku 2000 - 201?
+        <br>    &nbsp;
+        </div>
+        
+        <a href=""><h2>Ostatní</h2></a>
+        <div> Ostatní od roku 2000 - 201?
+        <br>    &nbsp;
+        </div>
+        
+        
+        
+    </body>
+</html>
