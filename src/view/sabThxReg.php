73216a37a593dda478b6792b65b35d6e238794ce
RadekOberreiter <oberreiterradek@gmail.com>
2013-06-06 13:38:13 +0300

    Update sabThxReg.php

--- src/view/sabThxReg.php
+++ src/view/sabThxReg.php
@@ -1,6 +1,6 @@
 <html>
     <head  lang="cs">
-        <title>Děkujeme za Registraci</title>
+        <title>Děkujeme za registraci</title>
         <meta http-equiv="refresh" content="5;url=../web/index.php">
         <meta charset="UTF-8">
         <link href="css/css1.css" rel="stylesheet" type="text/css" />
